946d7a11b1a207c4e2a503c40d61a8da22dface1
Georg Grauberger <georg.grauberger@gmail.com>
2022-07-24 20:09:42 +0300

    Add mac gui preferences

---
+++ roles/mac_gui/files/osx.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env zsh
+ 
+defaults write com.apple.dock "autohide" -bool "true" && killall Dock
+
+
+
+# Keyboard configuration
+defaults write NSGlobalDomain ApplePressAndHoldEnabled -bool false
+defaults write NSGlobalDomain InitialKeyRepeat -int 20
+defaults write NSGlobalDomain KeyRepeat -int 1
+
+
+# Finder preferences
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
+
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+defaults write com.apple.finder NewWindowTarget -string "PfHm"
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/"
+
+defaults write com.apple.finder ShowStatusBar -bool true
+
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
