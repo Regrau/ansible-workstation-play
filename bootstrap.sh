2b01b8a4c70b3d31512d500801dabfe3151987f0
Georg Grauberger <georg.grauberger@gmail.com>
2022-07-24 19:08:10 +0300

    Add dev dependencies to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,9 +11,12 @@ python3 -m venv ansible_venv
 source ansible_venv/bin/activate
 
 pip3 install wheel
+pip3 install --upgrade pip
 
 pip3 install ansible \
              ansible-core \
+             ansible-lint \
+             yamllint \
              molecule \
              molecule_vagrant
 
